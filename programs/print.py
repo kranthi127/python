824eeedf8ac1b5e32789c04000d6ddbe002f9376
kranthi127 <kranthi127eee@gmail.com>
2022-05-24 13:02:23 +0530

    general input output function are preformed

--- programs/print.py
+++ programs/print.py
@@ -1,1 +1,19 @@
 print("my frist python program")
+
+# to input the string we use input keyword
+n=input('enter the n value')
+print(n)
+#multiple inputs at single time 
+cash,pin=input('enter cash and then pin').split(',')
+print(cash,pin)
+#to print in the fromatted format
+print('cash{} pin{}'.format(cash,pin))
+#printing formats 
+print('kranthi is good boy ',sep="@" ,end='\n')
+print('hi')
+#to get the type of the input variable we type
+m=100
+print(type(m))
+#always the varable received from the input is string we need to convert required type
+n=int(input("entet the value"))
+print(type(n))
